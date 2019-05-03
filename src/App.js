5d1c1f58acca034f637b43260c0b6dfce2c18054
Christopher Iliyas <christopher.iliyas@andela.com>
2019-05-03 23:13:54 +0100

    wrote first test

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <p>This is the beginning of something very special.</p>
+        </div>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => {
-    return (
-        <div>
-            <p>This is the beginning of something very special.</p>
-        </div>
-    );
-};
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
