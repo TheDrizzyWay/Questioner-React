fe53d4123f23f431d8a45e8aa6b89450de7c5274
Christopher Iliyas <christopher.iliyas@andela.com>
2019-05-03 23:47:40 +0100

    setup enzyme testing

--- src/tests/unit-tests/App.test.js
+++ src/tests/unit-tests/App.test.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import App from '../../App';
 
-describe('App', () => {
-    it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<App />, div);
-        ReactDOM.unmountComponentAtNode(div);
-    });
-
-    test('has a valid snapshot', () => {
-        const component = renderer.create(
-            <App />);
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
+describe('<App />', () => {
+    it('Should render component successfully', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.length).toEqual(1);
     });
 });
