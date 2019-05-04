256fcb00eb5cdb560ec9879a7bb7840f6934868a
Christopher Iliyas <christopher.iliyas@andela.com>
2019-05-04 23:01:44 +0100

    finish styling landing page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 
-const ButtonComponent = ({ name, onClick, type, disabled, className, children }) => {
+const ButtonComponent = ({ name, onClick, type, disabled, className, children, variant, color }) => {
     return (
         <Button
             type={type}
@@ -10,10 +10,23 @@ const ButtonComponent = ({ name, onClick, type, disabled, className, children })
             disabled={disabled}
             onClick={onClick}
             className={className}
+            variant={variant}
+            color={color}
         >
             {children}
         </Button>
     );
+};
+
+ButtonComponent.propTypes = {
+    name: PropTypes.string,
+    onClick: PropTypes.func,
+    type: PropTypes.string.isRequired,
+    disabled: PropTypes.bool,
+    className: PropTypes.string,
+    children: PropTypes.string,
+    variant: PropTypes.string,
+    color: PropTypes.string
 };
 
 export default ButtonComponent;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NotFoundPage from '../containers/NotFoundPage';
+import LandingPage from '../containers/LandingPage';
 
 const Routes = () => {
     return (
     <>
       <Router>
           <Switch>
-              <Route exact path="/" />
+              <Route exact path="/" component={LandingPage}/>
               <Route component={NotFoundPage} />
           </Switch>
       </Router>
