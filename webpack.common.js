4cdf961e62f409cf6cbcaea6026c069fd8af782e
Christopher Iliyas <christopher.iliyas@andela.com>
2019-05-12 00:30:22 +0100

    add favicon

--- webpack.common.js
+++ webpack.common.js
@@ -47,6 +47,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: './index.html',
+            favicon: './src/assets/images/question.png',
             title: 'production'
         })
     ],
