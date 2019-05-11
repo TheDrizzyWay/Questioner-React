e11bf4abb0aa4f5ee813a5dd2da13806422537ac
Christopher Iliyas <christopher.iliyas@andela.com>
2019-05-11 23:55:43 +0100

    make header reusable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Header = () => {
+const Header = ({ children }) => {
     return (
         <div className="header_container">
             <header>
-                <h1>Questioner</h1>
+                <h1>{children}</h1>
             </header>
         </div>
     );
+};
+
+Header.propTypes = {
+    children: PropTypes.any
 };
 
 export default Header;
