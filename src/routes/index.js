ae804784540f784bfae8ebfc03ce89fb761ef56d
Andela <andela@Andelas-MacBook-Pro-2.local>
2019-05-17 23:25:02 +0100

    finish login page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SignUpPage from '../containers/SignUpPage';
+import LoginPage from '../containers/LoginPage';
 import NotFoundPage from '../containers/NotFoundPage';
 import LandingPage from '../containers/LandingPage';
 
@@ -11,6 +12,7 @@ const Routes = () => {
             <Switch>
                 <Route exact path="/" component={LandingPage} />
                 <Route exact path="/signup" component={SignUpPage} />
+                <Route exact path="/signin" component={LoginPage} />
                 <Route component={NotFoundPage} />
             </Switch>
         </Router>
